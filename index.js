d8f5fae5ad7885e9728cbd8101149b4fb14db65a
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2020-11-05 21:02:21 +0100

    refactored excercise 9
    Excercise 9
    did not dollow the exact assignment specification
    
    rebuild id assignment, their as values were illegal (an id cannot start with a number)

--- index.js
+++ index.js
@@ -14,12 +14,23 @@ window.onload = () => {
 		.then((body) => (catalog = body))
 		.then(() => {
 			loadLibrary(catalog)
+			if (window.location.search !== "") {
+				bigFunnyCard(window.location.search.substring(1))
+			}
 			library = [...catalog]
 		})
+
 	document.querySelector("#cart").addEventListener("click", loadCart)
 	document.querySelector("#library").addEventListener("click", reLoadLibrary)
 	document.querySelector("#skipped").addEventListener("click", loadSkipped)
 }
+
+const searchData = (asin) =>
+	fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${asin}`, {
+		method: "GET",
+	})
+		.then((response) => response.json())
+		.then((body) => body.items[0].volumeInfo)
 
 const reLoadLibrary = () => {
 	document.querySelector("#cart").removeEventListener("click", clearCart)
@@ -54,17 +65,20 @@ const loadLibrary = (shelf) => {
 		.forEach((card) => {
 			card.remove()
 		})
-	console.log(shelf)
+	//console.log(shelf)
 	shelf.forEach((book) => {
 		let card = template.cloneNode(true)
-		card.id = book.asin
+		card.id = `isbn${book.asin}`
 		card.querySelector(".card-title").innerHTML = `<b>${book.title}</b>`
 		card.querySelector(
-			".card-text"
-		).innerHTML = `category :<b>${book.category}</b><br/>price:<b>${book.price}</b> `
+			".book-category"
+		).innerHTML = `category :<b>${book.category}</b>`
+		card.querySelector(".book-price").innerHTML = `price:<b>${book.price}</b>`
 		card.querySelector(".card-img-top").src = book.img
 		card.querySelector(".btn-cart").addEventListener("click", toggleCartStatus)
 		card.querySelector(".btn-skip").addEventListener("click", toggleSkip)
+		//card.querySelector(".card-title").addEventListener("click", bigFunnyCard)
+		card.querySelector(".book-title").href = `?${card.id}`
 		card.classList.remove("d-none")
 		template.parentElement.appendChild(card)
 	})
@@ -74,11 +88,11 @@ const toggleCartStatus = (event) => {
 	const card = event.target.closest(".card")
 	card.classList.toggle("in-cart")
 	if (card.matches(".in-cart")) {
-		cart.push(catalog.find((book) => book.asin === card.id))
+		cart.push(catalog.find((book) => book.asin === card.id.substring(4)))
 		card.querySelector(".btn-cart").innerText = "remove from cart"
 	} else {
 		cart.splice(
-			cart.findIndex((book) => book.asin === card.id),
+			cart.findIndex((book) => book.asin === card.id.substring(4)),
 			1
 		)
 		card.querySelector(".btn-cart").innerText = "add to cart"
@@ -88,9 +102,9 @@ const toggleCartStatus = (event) => {
 const toggleSkip = (event) => {
 	const card = event.target.closest(".card")
 	card.remove()
-	skipped.push(catalog.filter((book) => book.asin === card.id))
+	skipped.push(catalog.find((book) => book.asin === card.id.substring(4)))
 	library.splice(
-		library.findIndex((book) => book.asin === card.id),
+		library.findIndex((book) => book.asin === card.id.substring(4)),
 		1
 	)
 	loadLibrary(library)
@@ -111,5 +125,26 @@ const search = (event) => {
 		) {
 			loadLibrary(library)
 		}
+	}
+}
+
+const bigFunnyCard = (id) => {
+	const card = document.querySelector(`#${id}`)
+	if (card.matches(".col-md-4")) {
+		Array.from(document.querySelectorAll("#bookShelf>:not(#cardTemplate)"))
+			.filter((book) => book.id !== card.id)
+			.forEach((card) => {
+				card.remove()
+			})
+
+		card.classList.toggle("col-md-4")
+		searchData(card.id.substring(4)).then((response) => {
+			const description = document.createElement("p")
+			description.innerText = response.description
+			card.querySelector(".book-description").appendChild(description)
+		})
+	} else {
+		card.classList.toggle("col-md-4")
+		loadLibrary(library)
 	}
 }
